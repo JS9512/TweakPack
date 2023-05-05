75ce13e02da54c059e49823eb6f82aefccc3eaaf
Jacopo Savore <118939761+Jaco9512@users.noreply.github.com>
2023-05-05 10:57:57 +0200

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -16,10 +16,26 @@ service call SurfaceFlinger 1022 f 1.5
 service call SurfaceFlinger 1008 i32 1
 
 # Set kernel parameters
-echo "50331648" > /proc/sys/vm/vm.dirty_bytes
-echo "16777216" > /proc/sys/vm/vm.dirty_background_bytes
-echo "400000" > /proc/sys/kernel/sched_min_granularity_ns
-echo "600000" > /proc/sys/kernel/sched_latency_ns
+echo 50331648 > /proc/sys/vm/vm.dirty_bytes
+echo 16777216 > /proc/sys/vm/vm.dirty_background_bytes
+echo 400000 > /proc/sys/kernel/sched_min_granularity_ns
+echo 600000 > /proc/sys/kernel/sched_latency_ns
+
+echo 50 > /dev/cpuctl/bg_non_interactive/cpu.shares
+echo 1000 > /dev/cpuctl/cpu.shares
+echo 1250 > /dev/cpuctl/fg_boost/cpu.shares
+
+echo 1000000 > /dev/cpuctl/cpu.rt_period_us 
+echo 800000 > /dev/cpuctl/cpu.rt_runtime_us
+
+echo 1000000 > /dev/cpuctl/apps/cpu.rt_period_us 
+echo 800000 > /dev/cpuctl/apps/cpu.rt_runtime_us
+
+echo 1000000 > /dev/cpuctl/bg_non_interactive/cpu.rt_period_us
+echo 800000 > /dev/cpuctl/bg_non_interactive/cpu.rt_runtime_us
+
+echo 1000000 > /dev/cpuctl/apps/bg_non_interactive/cpu.rt_period_us
+echo 800000 > /dev/cpuctl/apps/bg_non_interactive/cpu.rt_runtime_us
 
 # Trim trigger
 function trim() {

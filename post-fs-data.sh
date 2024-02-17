b260ccc9f20adb3eae01f5f6390a3fd0d0e91e6e
JS9512 <118939761+JS9512@users.noreply.github.com>
2024-02-17 17:50:54 +0000

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -6,30 +6,48 @@
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
+
 # This script will be executed in post-fs-data mode
 
+# ZERO LOGS
+stop tcpdump
+stop cnss_diag
 stop logd
-
-echo 0 > /d/mmc0/clk_delay
-
-echo 5 > /sys/class/mmc_host/mmc0/clk_scaling/up_threshold
-
-echo 75 > /sys/class/mmc_host/mmc0/clk_scaling/down_threshold
-
+stop statsd
+stop traced
+stop idd-logreader
+stop idd-logreadermain
+stop perfd
+stop performanced
+stop statscompanion
+echo 0 > /sys/kernel/tracing/tracing_on
 echo 0 > /sys/module/smem_log/parameters/log_enable
-
 echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_cmd
-
 echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_ctxt
-
 echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_drv
-
 echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_mem
-
 echo 0 > /sys/kernel/debug/kgsl/kgsl-3d0/log_level_pwr
 
+
+# Speed up I/O
+echo 0 > /d/mmc0/clk_delay
+echo 5 > /sys/class/mmc_host/mmc0/clk_scaling/up_threshold
+echo 75 > /sys/class/mmc_host/mmc0/clk_scaling/down_threshold
 echo 10000 > /sys/class/firmware/timeout
 
+
+# Extreme ZRAM
+total_ram_kb=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+total_ram_mb=$(echo "scale=0; $total_ram_kb /  1024" | bc)
+
+swapoff /dev/block/zram0 > /dev/null  2>&1
+echo  1 > /sys/block/zram0/reset
+echo  2 > /sys/block/zram0/max_comp_streams
+echo lz4 > /sys/block/zram0/comp_algorithm
+echo ${total_ram_mb}M > /sys/block/zram0/disksize
+mkswap /dev/block/zram0
+swapon /dev/block/zram0 -p  100
+echo  100 > /proc/sys/vm/swappiness
 
 
 # Set Adguard DNS

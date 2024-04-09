39d1e28072cd77952b5418015c29baa0c773eede
JS9512 <118939761+JS9512@users.noreply.github.com>
2024-04-09 10:46:39 +0200

    Update service.sh

--- service.sh
+++ service.sh
@@ -8,12 +8,13 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 
-# Increased Saturation for more vivid colors
+# Display Settings
+# Increase saturation for vivid colors
 service call SurfaceFlinger 1023 i32 1
 service call SurfaceFlinger 1022 f 1.5
 service call SurfaceFlinger 1015 i32 1 f  0.957 f  0 f  0 f  0 f  0 f  0.976 f  0 f  0 f  0 f  0 f  1 f  0 f  0 f  0 f  0 f  1
 
-# "Disable HW Overlay" Developer Option enabled for smoothness
+# Disable HW Overlay for smoothness
 service call SurfaceFlinger 1008 i32 1
 
 # Force 90 Hz refresh rate if available
@@ -22,13 +23,14 @@ service call SurfaceFlinger 1035 i32 0
 # Enable GPU-only composition profile
 service call SurfaceFlinger 1006 i32 0 i32 1
 
-# Set kernel parameters
+# Kernel Parameters
 echo 50331648 > /proc/sys/vm/dirty_bytes
 echo 16777216 > /proc/sys/vm/dirty_background_bytes
 echo 1048576 > /proc/sys/vm/max_map_count
 echo 400000 > /proc/sys/kernel/sched_min_granularity_ns
 echo 600000 > /proc/sys/kernel/sched_latency_ns
 
+# CPU Settings
 echo 50 > /dev/cpuctl/bg_non_interactive/cpu.shares
 echo 1000 > /dev/cpuctl/cpu.shares
 echo 1250 > /dev/cpuctl/fg_boost/cpu.shares
@@ -47,25 +49,25 @@ echo 800000 > /dev/cpuctl/apps/bg_non_interactive/cpu.rt_runtime_us
 
 # GPU Settings
 for gpu in /sys/class/kgsl/kgsl-3d0; do
-  echo 0 > $gpu/max_pwrlevel
-  echo 0 > $gpu/throttling
-  echo 0 > $gpu/bus_split
-  echo 1 > $gpu/force_clk_on
-  echo 1 > $gpu/force_bus_on
-  echo 1 > $gpu/force_rail_on
-  echo 1 > $gpu/force_no_nap
-  echo 4 > $gpu/devfreq/polling_interval
-  echo 10000000000 > $gpu/idle_timer
-  echo 0 > $gpu/snapshot/snapshot_crashdumper
-  echo 1 > $gpu/adrenoboost
-  echo 0 > $gpu/devfreq/adrenoboost
-  echo N > $gpu/adreno_idler_active
+ echo 0 > $gpu/max_pwrlevel
+ echo 0 > $gpu/throttling
+ echo 0 > $gpu/bus_split
+ echo 1 > $gpu/force_clk_on
+ echo 1 > $gpu/force_bus_on
+ echo 1 > $gpu/force_rail_on
+ echo 1 > $gpu/force_no_nap
+ echo 4 > $gpu/devfreq/polling_interval
+ echo 10000000000 > $gpu/idle_timer
+ echo 0 > $gpu/snapshot/snapshot_crashdumper
+ echo 1 > $gpu/adrenoboost
+ echo 0 > $gpu/devfreq/adrenoboost
+ echo N > $gpu/adreno_idler_active
 done
 
 # Apply VSync setting
 set start vsync
 
-# Network
+# Network Settings
 echo 0 > /proc/sys/net/ipv4/tcp_timestamps
 echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
 echo 1 > /proc/sys/net/ipv4/tcp_sack
@@ -98,7 +100,7 @@ echo 65536 > /proc/sys/net/core/optmem_max
 echo 8192 > /proc/sys/net/core/somaxconn
 echo 16384 > /proc/sys/net/core/netdev_max_backlog
 
-# Trim trigger
+# Trim Operations
 function trim() {
 fstrim -v $1 >> /cache/magisk.log
 }
@@ -113,3 +115,4 @@ trim /data
 trim /cache
 trim /persist
 trim /preload
+
